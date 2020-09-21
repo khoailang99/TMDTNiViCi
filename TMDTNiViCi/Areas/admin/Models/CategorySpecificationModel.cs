d2a7c572661775a8e3af2a13821b4a664dc7779b
khoailang99 <khoailang19101999@gmail.com>
2020-09-21 08:16:22 +0700

    Mục đích là sử dụng được model thông số kỹ thuật và model loại sản phẩm trong PVSpecification.

---
+++ TMDTNiViCi/Areas/admin/Models/CategorySpecificationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Areas.admin.Models
+{
+    public class CategorySpecificationModel
+    {
+        public IEnumerable<ProductCategory> CSCategory { get; set; }
+        public IEnumerable<PCSpecification> CSSpecification { get; set; }
+    }
+}
