e55f72a262742009c3b8b26822540c0a0856b99a
khoailang99 <khoailang19101999@gmail.com>
2020-09-21 08:12:18 +0700

    Được dùng để thiết lập các attribute áp dụng cho dropdownlist của loại sản phẩm.

---
+++ TMDTNiViCi/Common/AttributeProductTypeDDL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMDTNiViCi.Common
+{
+    /*AttributeProductTypeDDL chứa các attribute áp dụng cho dropdownlist của loại sản phẩm */
+    public class AttributeProductTypeDDL
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public AttributeProductTypeDDL(string Key, string Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+    }
+}
