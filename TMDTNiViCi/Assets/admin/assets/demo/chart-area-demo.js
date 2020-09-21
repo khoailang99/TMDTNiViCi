6047045f92eee80eeb5d72952da1b084ce886122
khoailang99 <khoailang19101999@gmail.com>
2020-09-21 08:01:43 +0700

    Các đồ thị biểu thị doanh thu, sản phẩm bán chạy ở homepage phía quản trị.

---
+++ TMDTNiViCi/Assets/admin/assets/demo/chart-area-demo.js
@@ -0,0 +1,139 @@
+﻿//// Set new default font family and font color to mimic Bootstrap's default styling
+//Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+//Chart.defaults.global.defaultFontColor = '#292b2c';
+
+//// Area Chart Example
+//var ctx = document.getElementById("myAreaChart");
+//var myLineChart = new Chart(ctx, {
+//  type: 'line',
+//  data: {
+//    labels: ["Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy", "Chủ nhật"],
+//    datasets: [{
+//      label: "Sessions",
+//      lineTension: 0.3,
+//      backgroundColor: "rgba(2,117,216,0.2)",
+//      borderColor: "rgba(2,117,216,1)",
+//      pointRadius: 5,
+//      pointBackgroundColor: "rgba(2,117,216,1)",
+//      pointBorderColor: "rgba(255,255,255,0.8)",
+//      pointHoverRadius: 5,
+//      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+//      pointHitRadius: 50,
+//      pointBorderWidth: 2,
+//      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274],
+//    }],
+//  },
+//  options: {
+//    scales: {
+//      xAxes: [{
+//        time: {
+//          unit: 'date'
+//        },
+//        gridLines: {
+//          display: false
+//        },
+//        ticks: {
+//          maxTicksLimit: 7
+//        }
+//      }],
+//      yAxes: [{
+//        ticks: {
+//          min: 0,
+//          max: 50000,
+//          maxTicksLimit: 5
+//        },
+//        gridLines: {
+//          color: "rgba(0, 0, 0, .125)",
+//        }
+//      }],
+//    },
+//    legend: {
+//      display: false
+//    }
+//  }
+//});
+
+var year = moment().year();
+var month = moment().month();
+var today = 24;
+var weekOrder = 1;
+var startDate = null, endDate = null;
+
+if (today < 8) {
+    startDate = new Date(year, month, 1);
+    endDate = new Date(year, month, 7);
+} else if (today < 15) {
+    weekOrder = 2;
+    startDate = new Date(year, month, 8);
+    endDate = new Date(year, month, 14);
+} else if (today < 22) {
+    weekOrder = 3;
+    startDate = new Date(year, month, 15);
+    endDate = new Date(year, month, 21);
+} else {
+    weekOrder = 4;
+    startDate = new Date(year, month, 22);
+    endDate = new Date(year, month + 1, 0);
+}
+
+startDate = moment(startDate).format("DD-MM-YYYY");
+endDate = moment(endDate).format("DD-MM-YYYY");
+
+Highcharts.chart('myAreaChart', {
+    chart: {
+        renderTo: 'myAreaChart',
+        type: 'column'
+    },
+    title: {
+        text: `Các ngày trong tuần ${weekOrder} từ ${startDate} đến ${endDate}`
+    },
+    tooltip: {
+        shared: true
+    },
+    xAxis: {
+        categories: [
+            'Thứ hai',
+            'Thứ ba',
+            'Thứ tư',
+            'Thứ năm',
+            'Thứ sáu',
+            'Thứ bảy',
+            'Chủ nhật'
+        ],
+        crosshair: true
+    },
+    yAxis: [{
+        title: {
+            text: 'Doanh thu'
+        },
+        max: 1000000000,
+        min: 0
+    }, {
+        title: {
+            text: 'Hóa đơn'
+        },
+        minPadding: 0,
+        maxPadding: 0,
+        max: 100000,
+        min: 0,
+        opposite: true,
+        labels: {
+            format: "{value}"
+        }
+    }],
+    series: [{
+        type: 'pareto',
+        name: 'Tổng hóa đơn',
+        yAxis: 1,
+        zIndex: 10,
+        data: [50000, 12000, 5000, 75000, 90000, 45000, 75000]
+    }, {
+        name: 'Doanh thu',
+        type: 'column',
+        zIndex: 2,
+        tooltip: {
+            valueSuffix: 'đ'
+        },
+        data: [50000000, 55000000, 22000000, 11000000, 700000000, 440000000, 510000000]
+    }]
+});
---
+++ TMDTNiViCi/Assets/admin/assets/demo/chart-bar-demo.js
@@ -0,0 +1,118 @@
+﻿//// Set new default font family and font color to mimic Bootstrap's default styling
+//Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+//Chart.defaults.global.defaultFontColor = '#292b2c';
+
+//// Bar Chart Example
+//var ctx = document.getElementById("myBarChart");
+//var myLineChart = new Chart(ctx, {
+//  type: 'bar',
+//  data: {
+//    labels: ["Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy", "Chủ nhật"],
+//    datasets: [{
+//      label: "Revenue",
+//      backgroundColor: "rgba(2,117,216,1)",
+//      borderColor: "rgba(2,117,216,1)",
+//      data: [4215, 5312, 6251, 7841, 9821, 14984],
+//    }],
+//  },
+//  options: {
+//    scales: {
+//      xAxes: [{
+//        time: {
+//          unit: 'month'
+//        },
+//        gridLines: {
+//          display: false
+//        },
+//        ticks: {
+//          maxTicksLimit: 6
+//        }
+//      }],
+//      yAxes: [{
+//        ticks: {
+//          min: 0,
+//          max: 15000,
+//          maxTicksLimit: 5
+//        },
+//        gridLines: {
+//          display: true
+//        }
+//      }],
+//    },
+//    legend: {
+//      display: false
+//    }
+//  }
+//});
+
+// Create the chart
+Highcharts.chart('myBarChart', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: `Top sản phẩm bán chạy nhất trong hôm nay ${moment().format("DD-MM-YYYY")}`,
+        align: 'center',
+        x: -10
+    },
+
+    accessibility: {
+        announceNewData: {
+            enabled: true
+        },
+        point: {
+            valueSuffix: ''
+        }
+    },
+
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [
+        {
+            colorByPoint: true,
+            data: [
+                {
+                    name: "Chrome",
+                    y: 62.74
+                },
+                {
+                    name: "Firefox",
+                    y: 10.57
+                },
+                {
+                    name: "Internet Explorer",
+                    y: 7.23
+                },
+                {
+                    name: "Safari",
+                    y: 5.58
+                },
+                {
+                    name: "Edge",
+                    y: 4.02
+                },
+                {
+                    name: "Opera",
+                    y: 1.92
+                },
+                {
+                    name: "Other",
+                    y: 7.62,
+                }
+            ]
+        }
+    ]
+});
+
