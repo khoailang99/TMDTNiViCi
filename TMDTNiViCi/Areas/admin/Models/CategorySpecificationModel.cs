0eb763193377687ab0cfa171974a1fde6be23f7c
khoailang99 <khoailang19101999@gmail.com>
2020-09-30 10:23:22 +0700

    Chuyển 1 model thành model cha chứa 2 model con, dùng trong hiển thị danh mục sp cũng như hiển thị, chỉnh sửa thông số.

--- TMDTNiViCi/Areas/admin/Models/CategorySpecificationModel.cs
+++ TMDTNiViCi/Areas/admin/Models/CategorySpecificationModel.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using TMDTNiViCi.Model.EF;
+using TMDTNiViCi.Model.Models;
 
 namespace TMDTNiViCi.Areas.admin.Models
 {
     public class CategorySpecificationModel
     {
         public IEnumerable<ProductCategory> CSCategory { get; set; }
-        public IEnumerable<PCSpecification> CSSpecification { get; set; }
+        public IEnumerable<ProductCategoryS_ProductS_Model> PCS_PS_Model { get; set; }
     }
 }
