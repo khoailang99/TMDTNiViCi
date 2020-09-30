b449ffb69c6085b37525c0b9b31b89b13f3a4d85
khoailang99 <khoailang19101999@gmail.com>
2020-09-30 10:09:02 +0700

    Model chứa 2 model con: PCSpecification và Product_Specification, được dùng để hiển thị thông số kĩ thuật và chỉnh sửa.

---
+++ TMDTNiViCi.Model/Models/ProductCategoryS_ProductS_Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Model.Models
+{
+    public class ProductCategoryS_ProductS_Model
+    {
+        public PCSpecification PCSModel { get; set; }
+
+        public Product_Specifications PSModel { get; set; }
+    }
+}
