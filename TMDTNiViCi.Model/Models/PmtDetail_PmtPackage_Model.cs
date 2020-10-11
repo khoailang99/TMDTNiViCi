56bf3b54d3b45589c6812846e8e12316d77017ca
khoailang99 <khoailang19101999@gmail.com>
2020-10-11 13:06:59 +0700

    Thêm model chứa gói khuyến mãi và chi tiết khuyến mãi.

---
+++ TMDTNiViCi.Model/Models/PmtDetail_PmtPackage_Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Model.Models
+{
+    public class PmtDetail_PmtPackage_Model
+    {
+        public PromotionPackage PmtPackage { get; set; }
+
+        public List<Promotion> PmtDetail { get; set; }
+    }
+}
