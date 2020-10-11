edc754bdd1bcc4fbd8858109333ab5eb3b8d5f2c
khoailang99 <khoailang19101999@gmail.com>
2020-10-11 19:45:43 +0700

    Thêm hàm lấy chi tiết khuyến mãi và gói khuyến mãi.

---
+++ TMDTNiViCi/Areas/admin/Controllers/PromotionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDTNiViCi.Model.Dao;
+using TMDTNiViCi.Model.Models;
+
+namespace TMDTNiViCi.Areas.admin.Controllers
+{
+    public class PromotionController : Controller
+    {
+        PromotionDao promotionDao = new PromotionDao();
+        // GET: admin/Promotion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public List<PmtDetail_PmtPackage_Model> Get_PmtDetail_PmtPackage_PC(int pmtPackageId)
+        {
+            return promotionDao.Get_PmtDetail_PmtPackage_Dao(pmtPackageId);
+        }
+    }
+}
