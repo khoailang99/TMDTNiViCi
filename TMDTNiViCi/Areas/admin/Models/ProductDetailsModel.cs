06745c2f606174a0dca187a1334f8e7c5f0e307d
khoailang99 <khoailang19101999@gmail.com>
2020-10-11 12:09:56 +0700

    Thêm model để chứa các thông tin về 1 sản phẩm.

---
+++ TMDTNiViCi/Areas/admin/Models/ProductDetailsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TMDTNiViCi.Model.EF;
+using TMDTNiViCi.Model.Models;
+
+namespace TMDTNiViCi.Areas.admin.Models
+{
+    public class ProductDetailsModel
+    {
+        public List<String> mainImages { get; set; }
+        public Product product { get; set; }
+
+        public Supplier supplier { get; set; }
+
+        public List<ProductCategory> productCategories { get; set; }
+
+        public List<PmtDetail_PmtPackage_Model> pmtDetail_PmtPackage_Models { get; set; }
+
+        public List<ProductCategoryS_ProductS_Model> PCS_PS_Model { get; set; }
+    }
+}
