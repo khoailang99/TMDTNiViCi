adc1122eb36b3785beb0b20d81d5528e6b7e1c54
khoailang99 <khoailang19101999@gmail.com>
2020-10-11 12:05:30 +0700

    Xử lý những gì  liên quán đến loại sản phẩm, danh mục.

---
+++ TMDTNiViCi/Areas/admin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDTNiViCi.Model.Dao;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Areas.admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryDao categoryDao = new CategoryDao();
+        // GET: admin/Category
+        public ActionResult Index()
+        {
+            return View(categoryDao.GetCategoriesPTDao(95, null));
+        }
+
+        public ProductCategory GetProductTypeCC(int PCId)
+        {
+            return categoryDao.GetProductTypeDao(PCId);
+        }
+
+        public List<ProductCategory> GetAllProdTypesAndCategoriesCC(bool boolGetAllProdTypes)
+        {
+            return categoryDao.GetAllProdTypesAndCategoriesDao(boolGetAllProdTypes);
+        }
+
+        public List<ProductCategory> GetCategoriesCC(int PCId, string selectedCategories = null)
+        {
+            return categoryDao.GetCategoriesPTDao(PCId, selectedCategories);
+        }
+    }
+}
