510eafb32dc824e4e44b3e334d456d814e20c821
khoailang99 <khoailang19101999@gmail.com>
2020-10-11 13:03:14 +0700

    Thêm hàm lấy nhà cung cấp.

---
+++ TMDTNiViCi.Model/Dao/SupplierDao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Model.Dao
+{
+    public class SupplierDao
+    {
+        TMDTNiViCiDbContext db = null;
+        char delimiter = char.Parse(",");
+        public SupplierDao()
+        {
+            db = new TMDTNiViCiDbContext();
+        }
+
+        public Supplier GetSupplierDao(int id)
+        {
+            return db.Suppliers.SingleOrDefault(s => s.ID == id);
+        }
+    }
+}
