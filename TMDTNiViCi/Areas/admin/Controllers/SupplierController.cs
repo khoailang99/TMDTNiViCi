35381c48f124fe65df7a9a7632542acddeb114bb
khoailang99 <khoailang19101999@gmail.com>
2020-10-13 08:50:47 +0700

    Thêm hàm lấy thông tin của 1 nhà cung cấp.

---
+++ TMDTNiViCi/Areas/admin/Controllers/SupplierController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDTNiViCi.Model.Dao;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Areas.admin.Controllers
+{
+    public class SupplierController : Controller
+    {
+        SupplierDao supplierDao = new SupplierDao();
+        // GET: admin/Supplier
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Supplier GetSupplierSC(int id)
+        {
+            return supplierDao.GetSupplierDao(id);
+        }
+    }
+}
