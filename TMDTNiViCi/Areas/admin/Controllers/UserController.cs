a22249862e1a0bcd87016df7c3f182b13b1c726c
khoailang99 <khoailang19101999@gmail.com>
2020-10-16 00:45:29 +0700

    Thêm hàm lấy thông tin 1 người dùng.

---
+++ TMDTNiViCi/Areas/admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDTNiViCi.Model.Dao;
+using TMDTNiViCi.Model.EF;
+
+namespace TMDTNiViCi.Areas.admin.Controllers
+{
+    public class UserController : Controller
+    {
+        UserDao userDao = new UserDao();
+        // GET: admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ApplicationUser GetAUserInfoUC(int id)
+        {
+            return userDao.GetAUserInfoDao(id);
+        }
+    }
+}
