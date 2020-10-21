700deae62ff103d537fc8c8e37a8679c31080f8a
khoailang99 <khoailang19101999@gmail.com>
2020-10-22 01:49:31 +0700

    Khởi tạo các đối tượng tương ứng vs 2 module dc import vào để chạy các method cần cần thiết thị khi tải trang.

---
+++ TMDTNiViCi/Assets/admin/js/Product/ShowAllProducts/Index.js
@@ -0,0 +1,23 @@
+﻿import ListProduct from "./ListProducts.js";
+import PDetail_PSpecification from "./ProductDetail.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    finishProgressBar();
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var objListProduct = new ListProduct(Number($(".prod-numb-search-filter").attr("data-numbProdSatisFilterCond")));
+    objListProduct.handlePaging();
+    objListProduct.f_displayProdNumb();
+    objListProduct.triggerClickEvPagingBtns();
+    objListProduct.triggerClickEvProdFilterBtn();
+    objListProduct.triggerClickEvProdSearchBtn();
+    objListProduct.triggerClickEventVEDBtn();
+    objListProduct.refreshProdFilterForm();
+
+    
+    var pDetail_pSpecification = new PDetail_PSpecification(objListProduct.maxHeight_PD_PS);
+    pDetail_pSpecification.triggerShownEOnModalProdDetail();
+
+
+}, false)
