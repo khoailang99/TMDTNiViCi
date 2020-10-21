5a3a8c262ce91dc09ab91f8b157de46c2aea41a4
khoailang99 <khoailang19101999@gmail.com>
2020-10-22 01:59:44 +0700

    Khởi tạo đối tượng AddProduct và chạy các method xử lý việc hiển thị, kích hoạt các sự kiện... khi tải trang thêm sản phẩm.

---
+++ TMDTNiViCi/Assets/admin/js/Product/Create/Index.js
@@ -0,0 +1,23 @@
+﻿import AddProduct from "./AddProduct.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    var objAddProduct = new AddProduct();
+    objAddProduct.handleResponsive();
+    objAddProduct.marksElemsNonBlank();
+    objAddProduct.changeBackgroundSpeTypeAP();
+    objAddProduct.triggerInlineEditor();
+    objAddProduct.getOriginalValueSelectTag();
+    objAddProduct.triggerChangeEvProdType();
+    objAddProduct.triggerClickEvImgSelectBtns();
+    objAddProduct.triggerChangeEvIUploadMulImgs();
+    objAddProduct.triggerClickEvBtnResetProdAddF();
+    objAddProduct.triggerClickEvSubmitBtnAPF();
+
+    // Phần cập nhật sản phẩm
+    objAddProduct.addCheckedToCategoryInput();
+    objAddProduct.displayMainARelatedImg();
+
+    window.onSuccessWAddProdKeyInfo = objAddProduct.onSuccessWAddProdKeyInfo.bind(objAddProduct);
+    window.onFailureWAddProdKeyInfo = objAddProduct.onFailureWAddProdKeyInfo;
+
+}, false)
